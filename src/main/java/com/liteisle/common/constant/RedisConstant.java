255894bb1fd88a0c094f604b3a0945b00c215c43
Rabbittank <moondust1196551872@gmail.com>
2025-07-23 09:10:48 +0800

    refactor:优化字段

--- src/main/java/com/liteisle/common/constant/RedisConstant.java
+++ src/main/java/com/liteisle/common/constant/RedisConstant.java
@@ -1,7 +1,7 @@
 package com.liteisle.common.constant;
 
 public class RedisConstant {
-    //TODO 替换
+
     public static final String USER_EMAIL = "user:mail:";
 
     public static final String BLACK_LIST_TOKEN = "black-list-token:";
@@ -11,8 +11,5 @@ public class RedisConstant {
     public static final String SHARE_TOKEN_BLOOM = "bloom:share-token:";
 
     public static final String SHARE_TOKEN_BLACKLIST_BLOOM = "bloom:share-token:blacklist:";
-
-    public static final String LOGIN_TOKEN_BLACKLIST_BLOOM = "bloom:login-token:blacklist:";
-
 
 }
